97527005bb1bd2f0b21868e6f5efcc5312912d87
unknown <honor334561@naver.com>
2020-01-13 17:58:33 +0900

    message 11

--- src/test/java/org/zerock/controller/BoardControllerTests.java
+++ src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -39,14 +39,14 @@ public class BoardControllerTests {
 				mockMvc.perform(MockMvcRequestBuilders.get("/board/list")).andReturn().getModelAndView().getModelMap()
 		);
 	}
-//	
-//	@Test
-//	public void testRegister() throws Exception{
-//		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register").
-//				param("title", "테스트 새글 제목").param("content", "테스트 새글 내용").param("writer", "user00")).andReturn().getModelAndView().getViewName();
-//		log.info("리절트 펲이지  : " + resultPage);
-//				
-//	}
+	
+	@Test
+	public void testRegister() throws Exception{
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/register").
+				param("title", "테스트 새글 제목").param("content", "테스트 새글 내용").param("writer", "user00")).andReturn().getModelAndView().getViewName();
+		log.info("리절트 펲이지  : " + resultPage);
+				
+	}
 	
 //	@Test
 //	public void testGet() throws Exception {
